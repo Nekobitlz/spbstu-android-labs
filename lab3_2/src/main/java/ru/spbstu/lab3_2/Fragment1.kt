477a8e289c5a96038a56049464d604e1aaffc242
Nekobitlz <jokers.bit@gmail.com>
2020-12-06 16:55:54 +0300

    3.5 replace about fragment by activity

--- lab3_2/src/main/java/ru/spbstu/lab3_2/Fragment1.kt
+++ lab3_2/src/main/java/ru/spbstu/lab3_2/Fragment1.kt
@@ -1,19 +1,16 @@
 package ru.spbstu.lab3_2
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import com.google.android.material.navigation.NavigationView
 
 class Fragment1 : Fragment() {
 
-    private lateinit var navController: NavController
     private lateinit var navigationView: NavigationView
     private lateinit var toSecondButton: Button
 
@@ -24,16 +21,14 @@ class Fragment1 : Fragment() {
     ): View = inflater.inflate(R.layout.activity_1, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        Log.d("LOVELY TAG", "recreated")
-        navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment)
         toSecondButton = view.findViewById(R.id.btn_to_second)
         navigationView = view.findViewById(R.id.nav_view)
 
         toSecondButton.setOnClickListener {
-            navController.navigate(R.id.action_fragment1_to_fragment2)
+            findNavController().navigate(R.id.action_fragment1_to_fragment2)
         }
         navigationView.setNavigationItemSelectedListener {
-            navController.navigate(R.id.action_fragment1_to_aboutFragment)
+            findNavController().navigate(R.id.action_fragment1_to_aboutActivity)
             return@setNavigationItemSelectedListener true
         }
     }
--- lab3_2/src/main/java/ru/spbstu/lab3_2/Fragment2.kt
+++ lab3_2/src/main/java/ru/spbstu/lab3_2/Fragment2.kt
@@ -1,19 +1,16 @@
 package ru.spbstu.lab3_2
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import com.google.android.material.navigation.NavigationView
 
 class Fragment2 : Fragment() {
 
-    private lateinit var navController: NavController
     private lateinit var navigationView: NavigationView
     private lateinit var toFirstButton: Button
     private lateinit var toThirdButton: Button
@@ -25,19 +22,19 @@ class Fragment2 : Fragment() {
     ): View = inflater.inflate(R.layout.activity_2, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment)
         toFirstButton = view.findViewById(R.id.btn_to_first)
         toThirdButton = view.findViewById(R.id.btn_to_third)
         navigationView = view.findViewById(R.id.nav_view)
 
+
         toFirstButton.setOnClickListener {
-            navController.navigate(R.id.action_fragment2_to_fragment1)
+            findNavController().navigate(R.id.action_fragment2_to_fragment1)
         }
         toThirdButton.setOnClickListener {
-            navController.navigate(R.id.action_fragment2_to_fragment3)
+            findNavController().navigate(R.id.action_fragment2_to_fragment3)
         }
         navigationView.setNavigationItemSelectedListener {
-            navController.navigate(R.id.action_fragment2_to_aboutFragment)
+            findNavController().navigate(R.id.action_fragment2_to_aboutActivity)
             return@setNavigationItemSelectedListener true
         }
     }
